6b3a53828783495d28e7ab1eeac12de687837b94
codenamedon <ddoricent97@gmail.com>
2025-05-13 19:58:59 -0400

    adding tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./*.html",         // all HTML files in your root
+    "./**/*.html",      // if you nest pages in folders
+    // add "./js/**/*.js" or "./src/**/*.{js,jsx}" if you have scripts or frameworks
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
